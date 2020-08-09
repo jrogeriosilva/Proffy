8c99daceac216345dcc82d804cab53f80c8b1a17
jrogeriosilva <joserogerio.net@gmail.com>
2020-08-09 18:18:14 -0300

    Create Teach-Item Component

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import PageHeader from "../../components/PageHeader";
-import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
+import TeacherItem from "../../components/TeacherItem";
 
 function TeacherList() {
   return (
@@ -27,32 +27,8 @@ function TeacherList() {
       </PageHeader>
 
       <main>
-        <article className="teacher-item">
-          <header>
-            <img
-              src="https://avatars2.githubusercontent.com/u/15113099?s=460&u=fcc4665021410c71d8d43e883109c585f3e7fd21&v=4"
-              alt="Rogério"
-            />
-            <div>
-              <strong>José Rogério</strong>
-              <span>Informática</span>
-            </div>
-          </header>
-          <p>
-            I love learn about Technology, and I'm currently working hard on
-            learn Typescript, Javascript ES6+,NodeJs, ReactJs and React Native.
-          </p>
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$ 60,00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="Whatsapp" />
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
+        <TeacherItem />
+        <TeacherItem />
       </main>
     </div>
   );
